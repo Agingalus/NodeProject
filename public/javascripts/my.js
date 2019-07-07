fbcf585961d2b292a60aa1d4fef13b1cfc417237
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-07-06 23:36:09 -0700

    took out extra stuff

--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -5,21 +5,21 @@
 /* eslint-disable no-undef */
 
 $(document).on("pagebeforeshow ", "#home", function() {
-    var info_view = ""; //string to put HTML in
-    $("#workDisplay").empty(); // since I do this everytime the page is redone, I need to remove existing before apending them all again
-    $.getJSON("./workList/") //Send an AJAX request
+
+    $("#workDisplay").empty();
+    $.getJSON("./workList/")
         .done(function(data) {
-            // make up each li as an <a> to the details-page
+
             $.each(data, function(index, record) {
                 $("#workDisplay").append("<li><a data-parm='" + record.WorkType + "'  href='#details-page'>" + record.WorkType + "</a></li>");
             });
 
 
-            $("#workDisplay").listview("refresh"); // need this so jquery mobile will apply the styling to the newly added li's
+            $("#workDisplay").listview("refresh");
 
-            $("a").on("click", function(event) { // set up an event, if user clicks any, it writes that items data-parm into the details page's html so I can get it there
+            $("a").on("click", function(event) {
                 var parm = $(this).attr("data-parm");
-                //do something here with parameter on  details page
+
                 $("#detailParmHere").html(parm);
 
             });
@@ -56,31 +56,10 @@ $(document).on("pagebeforeshow", "#details-page", function() {
 
 });
 
-// $(document).on("pagebeforeshow", "#deletePage", function() {
 
-//     $("#deleteWorkType").val("");
-// });
-
-// function deleteWork() {
-//     var work = $("#deleteWorkType").val();
-//     $("#deleteWorkType").val("");
-
-//     $.ajax({
-//         url: "/deleteWork/" + work,
-//         type: "DELETE",
-//         contentType: "application/json",
-//         success: function(response) {
-//             alert("The work successfully deleted in cloud");
-//         },
-//         error: function(response) {
-//             alert("ERROR: Note NOT deleted in cloud");
-//         }
-//     });
-// }
 
 function deleteWorkDetails() {
-    // var id = $("#deleteWorkType").val();
-    // $("#deleteWorkType").val("");
+
 
     var id = $("#detailParmHere").text();
     $.ajax({
@@ -145,25 +124,23 @@ function validData(work) {
 function addItem() {
     var date = new Date();
     var timestamp = date.getTime();
-    //var timestampString = timestamp.toString();
+
 
     var newName = $("#newName").val();
     var newWorkType = $("#newWorkType").val();
     var newStart = $("#newStart").val();
     var newEnd = $("#newEnd").val();
-    //var newTotalTime = $("#newTotalTime").val();
+
     var newPerHour = $("#newPerHour").val();
-    //var newTotalPay = $("#TotalPay").val();
+
     var newDateWorked = $("#newDateWorked").val();
     var newWork = {
         Name: newName,
         WorkType: newWorkType,
-        DateEntered: timestamp, //Date.now(),
+        DateEntered: timestamp,
         Start: newStart,
         End: newEnd,
-        //TotalTime: getTotalTime(newStart, newEnd),
         PerHour: newPerHour,
-        //TotalPay: newPerHour * this.TotalTime,
         DateWorked: newDateWorked
 
     };
@@ -197,14 +174,10 @@ function addItem() {
 $(document).on("pagebeforeshow", "#updatePage", function() {
 
     var textString = "fix me";
-    // let theDateEntered = new Date();
-    // let stringDate;
+
     var id = $("#detailParmHere").text();
     $.getJSON("/findWork/" + id)
         .done(function(data) {
-            //var parm = $(this).attr("data-parm");
-            //do something here with parameter on  details page
-            //$("#detailUpdateParmHere").html(parm);
 
             $("#updateName").val(data.Name);
             $("#updateWorkType").val(data.WorkType);
@@ -243,12 +216,9 @@ function updateItem() {
     var updateWork = {
         Name: updateName,
         WorkType: updateWorkType,
-
         Start: updateStart,
         End: updateEnd,
-
         PerHour: updatePerHour,
-
         DateWorked: updateDateWorked
 
     };
@@ -259,7 +229,6 @@ function updateItem() {
 
     if (validData(updateWork)) {
         $("#detailParmHere").html(updateWork.WorkType);
-
         $("#updateName").val("");
         $("#updateWorkType").val("");
         $("#updateDateEntered").val("");
@@ -269,7 +238,6 @@ function updateItem() {
         $("#updatePerHour").val("");
         $("#updateTotalPay").val("");
         $("#updateDateWorked").val("");
-
         $.ajax({
             url: "/updateWork/" + id,
             type: "PUT",
@@ -278,8 +246,7 @@ function updateItem() {
             data: JSON.stringify(updateWork),
             success: function(result) {
                 //alert("success");
-                // window.location.href = "#workList";
-                //$("#detailParmHere").val(updateWork.WorkType);
+
             }
         });
     } else {
@@ -291,9 +258,8 @@ function updateItem() {
 }
 
 function timeConverter(UNIX_timestamp) {
-    // if (isNaN(UNIX_timestamp)) {
-    //     return "no date";
-    // }
+
+
     var a = new Date(UNIX_timestamp); //* 1000);
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var year = a.getFullYear();
@@ -305,34 +271,3 @@ function timeConverter(UNIX_timestamp) {
     var time = date + " " + month + " " + year; //+ " " + hour + ":" + min + ":" + sec;
     return time;
 }
-
-// function parseTime(fullTime) {
-
-
-
-//     let hour = parseInt(fullTime.substring(0, 2));
-//     let min = parseInt(fullTime.substring(4, 6));
-//     return { hh: hour, mm: min };
-// }
-
-// function getTotalTime(start, end) {
-//     let startObj = parseTime(start);
-//     let endObj = parseTime(end);
-//     let totalTime = { hh: 0, mm: 0 };
-
-//     if (endObj.hh < startObj.hh) {
-//         endObj.hh += 24;
-//     }
-//     if (endObj.mm < startObj.mm) {
-//         endObj.hh--;
-//         endObj.mm += 60;
-//     }
-//     totalTime.hh = endObj.hh - startObj.hh;
-//     totalTime.mm = endObj.mm - startObj.mm;
-//     totalTime.mm = totalTime.mm / 60;
-//     return totalTime.hh + totalTime.mm;
-
-
-
-
-// }
